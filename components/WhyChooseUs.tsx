cb530c0f677bae490ef5460dac9faf8997ab077c
Serhii <124461383+nhnserg@users.noreply.github.com>
2024-08-10 00:08:05 +0200

    Update WhyChooseUs.tsx

--- components/WhyChooseUs.tsx
+++ components/WhyChooseUs.tsx
@@ -11,7 +11,7 @@ import Icon from "./Icon";
 const WhyChooseUs = () => {
   return (
     <div className="container mx-auto">
-      <h2 className="text-3xl font-semibold mb-12 ">
+      <h2 className="text-3xl font-semibold mb-10">
         Чому нас обирають
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
@@ -59,34 +59,37 @@ const WhyChooseUs = () => {
             </p>
           </div>
         </div>
-        <div className="flex">
-          <Icon
-            icon={Banknote}
-            size={100}
-            className="w-24 h-24 mr-4 text-red-500 stroke-1"
-          />
-          <div>
-            <h3 className="text-lg font-semibold mb-2">Доступність</h3>
-            <p className="text-gray-700">
-              Ми прагнемо запропонувати конкурентоспроможні ціни без шкоди для
-              якості.
-            </p>
+        {/* Обертка для центровки последних двух элементов с отступами */}
+        <div className="lg:col-span-3 flex justify-center lg:justify-between">
+          <div className="flex lg:ml-[209px]">
+            <Icon
+              icon={Banknote}
+              size={100}
+              className="w-24 h-24 mr-4 text-red-500 stroke-1"
+            />
+            <div>
+              <h3 className="text-lg font-semibold mb-2">Доступність</h3>
+              <p className="text-gray-700">
+                Ми прагнемо запропонувати конкурентоспроможні ціни без шкоди для
+                якості.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="flex">
-          <Icon
-            icon={Speech}
-            size={100}
-            className="w-24 h-24 mr-4 text-red-500 stroke-1"
-          />
-          <div>
-            <h3 className="text-lg font-semibold mb-2">
-              Професійна консультація
-            </h3>
-            <p className="text-gray-700">
-              Наші експерти завжди готові надати кваліфіковану допомогу у виборі
-              меблів.
-            </p>
+          <div className="flex lg:mr-[209px]">
+            <Icon
+              icon={Speech}
+              size={100}
+              className="w-24 h-24 mr-4 text-red-500 stroke-1"
+            />
+            <div>
+              <h3 className="text-lg font-semibold mb-2">
+                Професійна консультація
+              </h3>
+              <p className="text-gray-700">
+                Наші експерти завжди готові надати кваліфіковану допомогу у виборі
+                меблів.
+              </p>
+            </div>
           </div>
         </div>
       </div>
